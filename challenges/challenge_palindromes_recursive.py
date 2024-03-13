ae0944bdc87c4489a5e4d1568f8622d10a32f793
analaurafr <laurafernandes@outlook.com>
2024-03-13 14:01:28 -0300

    req 3 ok

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,16 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    # Caso base: string vazia é palíndromo
+    if word == "":
+        return False
     if low_index >= high_index:
         return True
-
-    # Verifica se os caracteres correspondentes são iguais
     if word[low_index] == word[high_index]:
-        # Chama recursivamente com os índices ajustados
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     else:
         return False
-
-
-# Função auxiliar para facilitar a chamada da função principal
-def is_palindrome(word):
-    return is_palindrome_recursive(word, 0, len(word) - 1)
